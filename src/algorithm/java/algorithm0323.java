149ac74fb211e300f0d733f4ec7c802583b7c8d7
imyeolmu <dladmswl3130@naver.com>
2022-03-23 22:24:38 +0900

    20220323_algorithm

---
+++ src/algorithm/java/algorithm0323.java
@@ -0,0 +1,15 @@
+package algorithm.java;
+//백준 10171번 고양이 문제
+public class algorithm0323 {
+	public static void main(String[] args) {
+		
+	
+	System.out.println("\\   /\\");
+	System.out.println(")  (')");
+	System.out.println("(  /  )");
+	System.out.println(" \\(__)|");  
+	
+	}
+}
+
+ // 역슬래쉬 \ 을 문자로 만들기 위해서는 \\두번 해야된다
