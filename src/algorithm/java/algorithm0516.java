14d1f7a72a04c4409483b39d446134245cc120b2
imeunji <dladmswl3130@naver.com>
2022-05-16 20:34:28 +0900

    Create algorithm0516.java

---
+++ src/algorithm/java/algorithm0516.java
@@ -0,0 +1,17 @@
+package java_study;
+
+
+public class JAVA0516 {
+	public static void main(String[] args) {
+		System.out.println("      ,r\'\"7"); //따옴표 출력은 /하고 하면된다
+		System.out.println("r'_ ,'   ,/");
+		System.out.println("\\.\".L_r'"); // 역슬래쉬 출력은 두번 //
+		System.out.println("`~\\/");
+		System.out.println( "   |");
+		System.out.println( "   |");
+	}
+}		
+	
+		
+		
+
