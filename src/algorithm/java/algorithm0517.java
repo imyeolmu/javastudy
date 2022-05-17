64e74a8cffaa5e9c667455541e5d2ae3f815d089
imeunji <dladmswl3130@naver.com>
2022-05-17 20:20:59 +0900

    Create algorithm0517.java

---
+++ src/algorithm/java/algorithm0517.java
@@ -0,0 +1,17 @@
+package java_study;
+
+import java.util.Scanner;
+//백준 1000번 문제 A+B만들기 
+public class java0510_1 {
+	public static void main(String[] args) {
+		
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int A = scan.nextInt();
+	int B = scan.nextInt();
+
+	System.out.println(A+B);
+  
+ }
+}
