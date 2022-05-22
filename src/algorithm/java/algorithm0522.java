5ad4cc47dca07b6a9c15bd0d7cf1d11a81b6347c
imeunji <dladmswl3130@naver.com>
2022-05-22 13:34:00 +0900

    Create algorithm0522.java

---
+++ src/algorithm/java/algorithm0522.java
@@ -0,0 +1,21 @@
+package algorithm.java;
+
+import java.util.Scanner;
+
+//백준알고리즘 10926번
+public class algorithm0521 {
+public static void main(String[] args) {
+    
+	Scanner in = new Scanner(System.in);
+    String str = in.nextLine();
+	System.out.println(str+"??!");
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
