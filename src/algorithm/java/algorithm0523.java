9dd2813859424cbf2cd520059680ab14df15a043
imeunji <dladmswl3130@naver.com>
2022-05-23 20:10:59 +0900

    Create algorithm0523.java

---
+++ src/algorithm/java/algorithm0523.java
@@ -0,0 +1,17 @@
+package algorithm.java;
+
+import java.util.Scanner;
+
+//백준알고리즘 18108번
+public class algorithm0523 {
+public static void main(String[] args) {
+    
+  Scanner sc = new Scanner (System .in);
+ 
+  System.out.println(sc.nextInt() - 543);
+}
+	
+
+
+	
+	}
