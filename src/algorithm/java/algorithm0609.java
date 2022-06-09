384177ad014e1c14a1df4de58b8dc6da4632c666
imeunji <101086601+imyeolmu@users.noreply.github.com>
2022-06-09 16:27:44 +0900

    Update algorithm0609.java

--- src/algorithm/java/algorithm0609.java
+++ src/algorithm/java/algorithm0609.java
@@ -1,1 +1,31 @@
+package javastudy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algorithm0609 {
+	
+	public static void main(String[] args) throws Exception, IOException {
+		
+	 BufferedReader br = null;
+
+	 br = new BufferedReader(new InputStreamReader(System.in));
+	 
+	 int n =Integer.parseInt(br.readLine());
+	 
+	 br.close(); // ╢щ╬фаж╠Б
+	 
+	 for(int i =1; i<n; i++) {
+		 for(int j =1; j<=i; j++) {
+			 System.out.println("*");
+		 }
+		 
+		 System.out.println();
+		 
+		 
+	 }
+	 
+ }
+
+}
