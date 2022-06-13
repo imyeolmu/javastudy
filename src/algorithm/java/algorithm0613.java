3400c9c461a1f846f028a82eaab96e203041c70d
imeunji <101086601+imyeolmu@users.noreply.github.com>
2022-06-13 21:24:03 +0900

    Create algorithm0613.java

---
+++ src/algorithm/java/algorithm0613.java
@@ -0,0 +1,27 @@
+package javastudy;
+
+
+import java.util.Scanner;
+
+public class algorithm0613 {
+	
+
+		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+	 
+			while(in.hasNext()) { // hasNext: boolean타입으로 반환된다 즉 true or false로 반환
+			//next()는 반환되는 타입의 차이 : 아무 타입으로 반환 가능하다
+			int A = in.nextInt();
+			int B = in.nextInt();//테스트 케이스를 입력받고 
+	  
+			
+			System.out.println(A+B);
+			
+			
+			
+
+			
+			}
+		}
+	
+	}
