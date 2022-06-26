fc0adb36b9564465c746c3dcf3b4e0b0ad3c5098
imeunji <101086601+imyeolmu@users.noreply.github.com>
2022-06-26 15:34:56 +0900

    Create algorithm0626.java

---
+++ src/algorithm/java/algorithm0626.java
@@ -0,0 +1,22 @@
+package javastudy;
+
+import java.io.IOException;
+
+public class algorithm0626 {
+	public static void main(String[] args) throws IOException {
+
+//	Scanner  in = new Scanner(System.in);
+		// scanner로 보통 입력 받지만 문제의 경우 아스키값만 출력하면 되기 때문에 scanner으로 받을 필요가 없다
+//	String N = in.next();
+//	
+//	String N = 
+//	
+//	System.out.println();
+//	
+//	
+
+		int a = System.in.read(); // 예외 처리를 해야한다
+		System.out.print(a);
+
+	}
+}
