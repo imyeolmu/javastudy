58c6263697a606f2f8d0dff59703252e3358bf10
imeunji <dladmswl3130@naver.com>
2022-09-28 21:25:08 +0900

    docs: 별찍기

--- src/프로그래머스 공부/09.23~09.30.java
+++ src/프로그래머스 공부/09.23~09.30.java
@@ -140,3 +140,20 @@ public class GugudanClass {
 		}
 	}
 }
+
+
+
+// 별 찍기 
+public class GugudanClass {
+	
+	public static void main(String[] args) {
+
+for(int i=0;i<4;i++){
+	for(int j=0;j<3-i;j++){
+		System.out.print(" "); 
+	}
+	for(int j=0;j<2*i+1;j++){
+		System.out.print("*"); 
+	}
+	System.out.println("");
+} 
